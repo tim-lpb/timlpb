01b342c4741de3a9e8722165dd00f08d0ce47b08
tim-lpb <tbernards@arcor.de>
2026-03-31 16:30:16 +0200

    Added avatar picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
     return (
         <>
             <div className="flex flex-1 flex-col">
                 <div className="flex-1 overflow-y-auto">
                     <div className="text-primary mx-auto flex max-w-2xl flex-1 flex-col gap-8 py-16 leading-[1.6] sm:py-32">
+                        <Image
+                            src="/img/avatar.jpeg"
+                            alt="Avatar of Tim"
+                            width={60}
+                            height={60}
+                            draggable={false}
+                            className="mb-8 rounded-full select-none"
+                        />
 
                         <h1 className="text-2xl font-semibold">
                             Tim
